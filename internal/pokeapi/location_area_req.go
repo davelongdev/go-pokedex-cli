98c94cdd9939f80511731868bcf708ee5af3f73a
dave long <davelongdev@gmail.com>
2024-09-18 21:58:39 -0400

    end of day backup - working on api interaction

---
+++ internal/pokeapi/location_area_req.go
@@ -0,0 +1,45 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"golang.org/x/tools/go/analysis/passes/defers"
+)
+
+func (c *Client) ListLocationAreas() (LocationAreasResponse, error){
+	endpoint := "/location-area"
+	fullURL := baseURL + endpoint
+
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return LocationAreasResponse{}, err
+	}
+	
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return LocationAreasResponse{}, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode > 399 {
+		return LocationAreasResponse{}, fmt.Errorf("bad status code: %v", resp.StatusCode)
+	}
+	
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return LocationAreasResponse{}, err
+	}
+	
+	locationAreasResp := LocationAreasResponse{}
+	err = json.Unmarshal(dat, &locationAreasResp)
+
+	if err != nil {
+		return LocationAreasResponse{}, err
+	}
+
+	return locationAreasResp, nil
+}
